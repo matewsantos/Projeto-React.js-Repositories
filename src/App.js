1b79aa7d2df38e4e5e3bc3d9c6d31dce649eef0a
mateu <mateus.rodriguesantos@hotmail.com>
2020-04-10 20:16:14 -0300

    Importando projeto para o GitHub

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, [])
+
+  async function handleAddRepository() {
+
+    const response = await api.post('/repositories', {
+      title: `Meu novo Repository ${Date.now()}`,
+      url: "https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs",
+      techs: "javascript Full Stack, Java, Python"
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    api.delete(`/repositories/${id}`);
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
+    repositories.splice(repositoryIndex, 1);
+
+    setRepositories([...repositories]);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository =>
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+          </li>
+        )}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
